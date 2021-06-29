38ce1210dd8b574b41893ac4d7579960509a7324
sailesh <sailesh@pop-os.localdomain>
2021-06-29 13:59:52 +0530

    2:00 jun 29

--- app/urls.py
+++ app/urls.py
@@ -1,8 +1,6 @@
 from django.urls import path
+
 from . import views
 urlpatterns = [
-    path("lipika", views.lipika),
-    path("sailesh", views.sailesh),
-    path("nagu", views.nagu),
-    path("arshath", views.arshath),
+    path("index", views.index),
 ]
--- app/views.py
+++ app/views.py
@@ -1,11 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
-def lipika(request):
-    return render(request,"app/lipika.html")
-def sailesh(request):
-    return render(request,"app/sailesh.html")
-def nagu(request):
-    return render(request,"app/nagu.html")
-def arshath(request):
-    return render(request,"app/arshath.html")
+
+
+def index(request):
+    return render(request, "app/index.html")
