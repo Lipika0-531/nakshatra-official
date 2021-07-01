f6dbb8d7d6c40c22d3616fefc96fe5c724319cee
sailesh <sailesh@pop-os.localdomain>
2021-07-01 23:01:59 +0530

    11:01 jul 1

--- app/views.py
+++ app/views.py
@@ -7,3 +7,5 @@ def index(request):
     return render(request, "app/index.html")
 def contact(request):
     return render(request, "app/contact.html")
+def work(request):
+    return render(request, "app/work.html")
