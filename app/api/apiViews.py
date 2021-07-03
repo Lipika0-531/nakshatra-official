49379e990d2c364a854a646bbb8d46589f45c5a1
rithin <rithin316@gmail.com>
2021-07-03 21:58:47 +0530

    full crud pre-migrate

--- app/api/apiViews.py
+++ app/api/apiViews.py
@@ -16,12 +16,35 @@ def new(request):
     return render(request, "app/API/products/new.html",{"form":form})
 
 
-def edit(request):
-    pass
+def edit(request, id):
+    if id:
+        product = models.Products.objects.filter(id=id).first()
+    form = apiForms.NewProductForm(request.POST or None, request.FILES or None,instance=product)
+    if request.POST and form.is_valid():
+        form.save()
+        return redirect('showAll')
 
-def show(request):
-    pass
+    context = {"form":form, "product":product}
+    return render(request, "app/API/products/edit.html",context)
 
+def delete(request, id):
+    if id:
+        models.Products.objects.filter(id=id).delete()
+    return redirect("showAll")
+
+def all(request):
+    products = models.Products.objects.all().order_by('category')
+    data=[]
+    temp=[]
+    initial_category = products[0].category
+    for product in products:
+        if product.category != initial_category:
+            data.append(temp)
+            temp=[]
+            initial_category = product.category
+        temp.append(product)
+
+    return render(request, "app/API/products/products.html",{"products":data})
 
 
 
--- app/urls.py
+++ app/urls.py
@@ -5,9 +5,9 @@ from .api import apiViews
 
 urlpatterns=[
     path("product/new/", apiViews.new, name="newProduct"),
-    path("products/<int:id>", apiViews.show, name="showProduct"),
+    path("products/<int:id>", apiViews.delete, name="deleteProduct"),
     path("products/edit/<int:id>/", apiViews.edit, name="editProduct"),
-    
+    path("products/", apiViews.all, name="showAll"),
     path("api/product/<int:id>", apiViews.productsApi.as_view(), name="productDetails"),
 ]
 
